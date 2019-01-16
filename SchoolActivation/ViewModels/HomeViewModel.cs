60aa58d5fba5ca52094f0d302a7dcf3a0b47bfe0
Usman Khalid <mianusman@msn.com>
2019-01-16 22:26:01 +0800

    initial commit

---
+++ SchoolActivation/ViewModels/HomeViewModel.cs
@@ -0,0 +1,15 @@
+﻿using SchoolActivation.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SchoolActivation.ViewModels
+{
+    public class HomeViewModel
+    {
+        public List<Pie> Pies { get; set; }
+        public string Title { get; set; }
+
+    }
+}
